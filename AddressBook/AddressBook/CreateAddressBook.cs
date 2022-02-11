722c46f6aad637e78d376729dc6007dad3c6baa7
akshata2005 <akshatasawant1805@gmail.com>
2022-02-11 18:58:10 +0530

    [Akshata] Add: created Addressbook

---
+++ AddressBook/AddressBook/CreateAddressBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class CreateAddressBook : IAddressBook
+    {
+        private Dictionary<string, Contacts> addressBook = new Dictionary<string, Contacts>();
+        public void AddContact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string emailAddress)
+        {
+            Contacts contact = new Contacts();
+            contact.FirstName = firstName;
+            contact.LastName = lastName;
+            contact.Address = address;
+            contact.City = city;
+            contact.State = state;
+            contact.Zip = zip;
+            contact.Email = emailAddress;
+            contact.PhoneNumber = phoneNumber;
+            addressBook.Add(contact.FirstName, contact);
+        }
+        public void DisplayContact()
+        {
+            foreach (KeyValuePair<string, Contacts> pair in addressBook)
+            {
+                Console.WriteLine("First Name : " + pair.Value.FirstName);
+                Console.WriteLine("Address : " + pair.Value.Address);
+                Console.WriteLine("City : " + pair.Value.City);
+                Console.WriteLine("State : " + pair.Value.State);
+                Console.WriteLine("Zip : " + pair.Value.Zip);
+                Console.WriteLine("Phone Number : " + pair.Value.PhoneNumber);
+                Console.WriteLine("Email : " + pair.Value.Email + "\n");
+            }
+   
+        }
+    }
+}
---
+++ AddressBook/AddressBook/IAddressBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    interface IAddressBook
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string emailAddress);
+        public void DisplayContact();
+    }
+}
